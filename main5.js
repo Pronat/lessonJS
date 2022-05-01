0a1cdd5c4990ef058f9cade68ec79dbb6864080c
Pronat <Fotolinza@gmail.com>
2022-05-01 23:01:29 +0300

    lesson5-4

--- main5.js
+++ main5.js
@@ -13,3 +13,38 @@ const compFn = (a, b) => {
     return -100
 }
 console.log([...numbers].sort((a, b)=>b - a))  //сортируем числа, а не строки
+
+const students = [
+    {
+        name: "Bob",
+        age: 22,
+        isMarried: true,
+        scores: 85,
+    },
+    {
+        name: "Alex",
+        age: 21,
+        isMarried: true,
+        scores: 89,
+    },
+    {
+        name: "Nick",
+        age: 20,
+        isMarried: false,
+        scores: 120,
+    },
+    {
+        name: "John",
+        age: 19,
+        isMarried: false,
+        scores: 100,
+    },
+    {
+        name: "John",
+        age: 19,
+        isMarried: false,
+        scores: 105,
+    },
+]
+console.log([...students].sort((a, b) => a.age - b.age))
+console.log([...students].sort((a, b) => a.name < b.name ? -1 : 1))
