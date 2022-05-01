68e4660ddb4e80217c601bc349da1fa6973763a1
Pronat <Fotolinza@gmail.com>
2022-05-01 21:37:54 +0300

    lesson5-3

--- main5.js
+++ main5.js
@@ -1,5 +1,15 @@
 console.log(22)
 
-const names = ["Donald", "Fedor", "Bob", "Alex",]
+const names = ["Donald", "Fedor", "Bob", "Alex", "101", "Игорь"]
 console.log(names.sort())
 console.log([...names].sort())
+
+const numbers = [1, 2, 33, 4, 5]
+console.log(numbers.sort())
+const compFn = (a, b) => {
+    if(a > b) {
+        return 999
+    }
+    return -100
+}
+console.log([...numbers].sort((a, b)=>b - a))  //сортируем числа, а не строки
