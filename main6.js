47d32d91e58e5f762ad4c404a079022409d67d0a
Pronat <Fotolinza@gmail.com>
2022-05-23 22:54:04 +0300

    lesson6-4

--- main6.js
+++ main6.js
@@ -1,10 +1,41 @@
 console.log(22)
 
-const sm = document.getElementById("small")
-const onClickHandler = () => alert("YO!!!")
+const students = [
+    {
+        name: "Bob",
+        age: 22,
+        isMarried: true,
+        scores: 85,
+    },
+    {
+        name: "Alex",
+        age: 21,
+        isMarried: true,
+        scores: 89,
+    },
+    {
+        name: "Nick",
+        age: 20,
+        isMarried: false,
+        scores: 120,
+    },
+    {
+        name: "John",
+        age: 19,
+        isMarried: false,
+        scores: 100,
+    },
+    {
+        name: "John",
+        age: 19,
+        isMarried: false,
+        scores: 105,
+    },
+]
 
-// sm.onclick = onClickHandler
-// sm.onclick = null
+const user = {
+    name: "Bob",
+        age: 23,
+    friends: ["Alex", "Nick", "John"]
+}
 
-sm.addEventListener('click', onClickHandler);
-sm.removeEventListener('click', onClickHandler);
