9f016d392605654638e41c488606846f03249053
Pronat <Fotolinza@gmail.com>
2022-05-23 21:21:19 +0300

    lesson6-3

--- main6.js
+++ main6.js
@@ -3,7 +3,8 @@ console.log(22)
 const sm = document.getElementById("small")
 const onClickHandler = () => alert("YO!!!")
 
-sm.onclick = onClickHandler
-sm.onclick = null
+// sm.onclick = onClickHandler
+// sm.onclick = null
 
-const bj = {}
+sm.addEventListener('click', onClickHandler);
+sm.removeEventListener('click', onClickHandler);
