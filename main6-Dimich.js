c56a3f335e507622b5a1c513929e2c85dd4fe5f1
Pronat <Fotolinza@gmail.com>
2022-05-25 23:35:52 +0300

    lesson6-samuray-7

--- main6-Dimich.js
+++ main6-Dimich.js
@@ -17,9 +17,20 @@ let state = {
 }
 
 let b = {
-    ...state,
-    age: 2,                //скопировали age и заменили его с 1 на 2
+    ...state,          //поверхностная копия объекта с помощью spred оператора
+    age: 2,          //скопировали age и заменили его с 1 на 2
     school: {
         ...state.school,   //скопировали address
-        director: { ...state.school.director } //скопировали director, который находится глубже
+        director: { ...state.school.director }   //скопировали director, который находится глубже
 }}
+
+
+
+let arr = [1,2,3,4,5]
+let brr = [0, ...arr, 7]   //поверхностная копия массива с помощью spred оператора и добавили в него 0 в начало и 7 в конце
+
+
+let arr2 = [{name: 'D'}, {name: 'M'}]
+let brr2 = [...arr2]
+brr2[0].name = 'Alex'
+console.log(arr2[0].name)
