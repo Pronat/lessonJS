b9aec8a0a2ac5e6320e9c536dd2fb57d1583fb28
Pronat <Fotolinza@gmail.com>
2022-05-25 22:49:50 +0300

    lesson6-samuray-4

--- main6-samuray.js
+++ main6-samuray.js
@@ -9,7 +9,7 @@ let a = {
     maxStudentsCount: 10,
     isOnline: true,
     students: ['ivan', 'andrey', 'farid'],
-    classsroom: {
+    classroom: {
         teacher: {
             name: 'ororo',
             age: 18
@@ -17,15 +17,21 @@ let a = {
     }
 }
 let b = {...a}
-b.classsroom.teacher.name = 'Alexander'
-console.log(a === b)
-console.log(a.students === b.students)
 
-a.students[0]= 'Alex'
-console.log(b.students[0])
-
-b.classsroom.teacher.age = 46
-console.log(a.classsroom.teacher.age)
+b.classroom.teacher.age = 46
+console.log(a.classroom.teacher.age)
 
 a.name = "Atatat"
 console.log(a.name === b.name)
+
+b.students = [...a.students]
+a.students[0]= 'Alex'
+console.log(a.students[0])
+
+b.classroom = {...a.classroom}
+b.classroom.teacher = {...a.classroom.teacher}
+b.classroom.teacher.name = {...a.classroom.teacher.name}
+b.classroom.teacher.name = 'Alexander'
+console.log(a === b)
+console.log(a.students === b.students)
+console.log(b.classroom.teacher.name)
