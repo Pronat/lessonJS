bbdfd190b2bc07c41c018f69dd4702c8cee5fa3f
Pronat <Fotolinza@gmail.com>
2022-05-25 22:39:00 +0300

    lesson6-samuray-3

--- main6-samuray.js
+++ main6-samuray.js
@@ -20,3 +20,12 @@ let b = {...a}
 b.classsroom.teacher.name = 'Alexander'
 console.log(a === b)
 console.log(a.students === b.students)
+
+a.students[0]= 'Alex'
+console.log(b.students[0])
+
+b.classsroom.teacher.age = 46
+console.log(a.classsroom.teacher.age)
+
+a.name = "Atatat"
+console.log(a.name === b.name)
