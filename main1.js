98c47d9cc388371b9573ec4f2a99194ba4e67b3e
AlexPron <Fotolinza@gmail.com>
2023-02-08 18:12:55 +0300

    less1-3

--- main1.js
+++ main1.js
@@ -1,8 +1,17 @@
-console.log(21)
-//примитивы: string, number, boolean, undefined, null, NaN, Infinity, Symbol, BigInt
+
+//примитивы: string, number, boolean, undefined, null, NaN(not a number), Infinity(попытка деления
+// на ноль) , Symbol, BigInt
 //объекты: array, function, objects
 
-// отличия:
+// Отличия:
 //    1. Более сложная структура
 //    2. Имеют свойства и методы
 //    3. Ссылочный тип данных
+console.log(typeof null)   //выдаст object
+console.log(typeof NaN)   //выдаст number
+console.log(typeof Infinity)   //выдаст number
+
+console.log(typeof (() => {}))   //выдаст function
+console.log(typeof [])   //выдаст object
+console.log(Array.isArray([]))   //выдаст true, вот так определяем, массив ли это
+
