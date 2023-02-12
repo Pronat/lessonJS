0e127cad672bea77df91f273cf77d427fbabe464
AlexPron <Fotolinza@gmail.com>
2023-02-12 23:17:01 +0300

    less2-1

--- ekzamen2.js
+++
@@ -1,3 +0,0 @@
-console.log(24)
-const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
-alert(users)
---
+++ main2ArrayMethods.js
@@ -0,0 +1,28 @@
+console.log(11111)
+
+let students = [
+    {
+        name: 'Bob',
+        age: 21,
+        isMarried: true,
+        scores: 85,
+    },
+     {
+        name: 'Alex',
+        age: 24,
+        isMarried: false,
+        scores: 100,
+    },
+     {
+        name: 'Gordon',
+        age: 55,
+        isMarried: false,
+        scores: 12,
+    },
+     {
+        name: 'Peter',
+        age: 78,
+        isMarried: true,
+        scores: 10,
+    },
+]
