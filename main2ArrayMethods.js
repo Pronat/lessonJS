b74e35d4c66bdb901d0d7ab0013a1fd4e50b1e1b
AlexPron <Fotolinza@gmail.com>
2023-02-12 23:27:59 +0300

    less2-2

--- main2ArrayMethods.js
+++ main2ArrayMethods.js
@@ -7,22 +7,35 @@ let students = [
         isMarried: true,
         scores: 85,
     },
-     {
+    {
         name: 'Alex',
         age: 24,
         isMarried: false,
         scores: 100,
     },
-     {
+    {
         name: 'Gordon',
         age: 55,
         isMarried: false,
         scores: 12,
     },
-     {
+    {
         name: 'Peter',
         age: 78,
         isMarried: true,
         scores: 10,
     },
 ]
+const GetNames = (array) => {
+    return (
+        array.map((el) => el.name)
+    )
+}
+const GetNames2 = (array) => {
+    let array1 = []
+    for (let i = 0; i < array.length; i++) {
+        array1.push(array[i].name)
+    }
+    return array1
+}
+console.log(GetNames2(students))
