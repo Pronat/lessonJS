4b78c161b4e966cbe10d29deefcb1b4b0cd14de1
AlexPron <Fotolinza@gmail.com>
2023-02-22 22:50:43 +0300

    less3-2

--- main3Event.js
+++ main3Event.js
@@ -13,4 +13,9 @@ sm.onclick = handler2 //перезатерли handler1
 sm.onclick = null   //обнулили значение
 
 sm.addEventListener('click', handler1)
-sm.addEventListener('click', handler2)  //это новый метод, handler1 не перезатерли
+sm.addEventListener('click', handler2)  //это новый метод, handler1 не перезатерли, сработают поочереди
+sm.removeEventListener('click', handler1)  //удалили handler1
+
+// всплытие, более распространено (99 %). Событие произошло в самом внутреннем элементе, а потом начало подниматься наружу.
+// погружение
+
