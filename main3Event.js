f0d38f283135b27d5d1810c180d75451ed934f92
AlexPron <Fotolinza@gmail.com>
2023-02-22 22:35:53 +0300

    less2-8

--- main3Event.js
+++ main3Event.js
@@ -1,2 +1,16 @@
 // handler === listener === subscriber => function    //все эти значения - функция
-// callback
+// callback => handler, setTimeout/setInterval, param of array method  //это все callback
+
+const sm = document.getElementById('small')
+const handler1 = (e) => {
+    console.log(e)
+}
+const handler2 = (e) => {
+    console.log(e)
+}
+sm.onclick = handler1 //назначили обработчик, это устаревший метод
+sm.onclick = handler2 //перезатерли handler1
+sm.onclick = null   //обнулили значение
+
+sm.addEventListener('click', handler1)
+sm.addEventListener('click', handler2)  //это новый метод, handler1 не перезатерли
