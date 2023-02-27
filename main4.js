dafafe50f1d8558f2c7facf5dd2b9a01cdd18ba2
AlexPron <Fotolinza@gmail.com>
2023-02-27 22:18:15 +0300

    main4-1

---
+++ main4.js
@@ -0,0 +1,17 @@
+console.log('Js lesson 4')
+
+export const todolists = [
+    {
+        id: "3243-343",
+        title: "Name 1",
+        filter: "all",
+        tasks: [
+            {id: 1, title: "HTML", isDone: false},
+            {id: 2, title: "CSS", isDone: false},
+            {id: 3, title: "React", isDone: false},
+        ]
+    },
+    {
+
+    },
+]
