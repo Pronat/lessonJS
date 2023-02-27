6cea68341657d6c24118368ce6aafcbaa377bb80
AlexPron <Fotolinza@gmail.com>
2023-02-27 22:25:36 +0300

    main4-2

--- main4.js
+++ main4.js
@@ -3,7 +3,7 @@ console.log('Js lesson 4')
 export const todolists = [
     {
         id: "3243-343",
-        title: "Name 1",
+        title: "Todolist Name 1",
         filter: "all",
         tasks: [
             {id: 1, title: "HTML", isDone: false},
@@ -12,6 +12,27 @@ export const todolists = [
         ]
     },
     {
-
+        id: "343443-34355",
+        title: "Todolist Name 2",
+        filter: "all",
+        tasks: [
+            {id: 4, title: "HTML2", isDone: false},
+            {id: 5, title: "CSS2", isDone: false},
+            {id: 6, title: "React2", isDone: false},
+        ]
     },
 ]
+
+const tasks = {
+    "Todolist Name 1": [
+    {id: 4, title: "HTML2", isDone: false},
+    {id: 5, title: "CSS2", isDone: false},
+    {id: 6, title: "React2", isDone: false},
+],
+    "Todolist Name 2": [
+        {id: 4, title: "HTML2", isDone: false},
+        {id: 5, title: "CSS2", isDone: false},
+        {id: 6, title: "React2", isDone: false},
+    ],
+
+}
