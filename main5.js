88a82df15249ecccef6a1dd0a5a0a24ae4e3b582
AlexPron <Fotolinza@gmail.com>
2023-03-07 17:13:30 +0300

    main5-2

--- main5.js
+++ main5.js
@@ -1,2 +1,5 @@
 console.log("Main5")
 
+const name = ["Onix", "Alex", "Fred", "alex", "aLex", "игорь", "Юрий"]
+// Сортировка
+console.log(name.sort()===name) //true, работает мутабельно, возвращает ссылку на массив
