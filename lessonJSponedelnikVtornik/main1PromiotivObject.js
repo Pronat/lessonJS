97e95f63a844645d6b31c393e8e6b0970b7752ad
AlexPron <Fotolinza@gmail.com>
2023-04-02 23:24:12 +0300

    main9-2

---
+++ lessonJSponedelnikVtornik/main1PromiotivObject.js
@@ -0,0 +1,25 @@
+
+//примитивы: string, number, boolean, undefined, null, NaN(not a number), Infinity(попытка деления
+// на ноль) , Symbol, BigInt
+//объекты: array, function, objects
+
+// Отличия:
+//    1. Более сложная структура
+//    2. Имеют свойства и методы
+//    3. Ссылочный тип данных
+console.log(typeof null)   //выдаст object
+console.log(typeof NaN)   //выдаст number
+console.log(typeof Infinity)   //выдаст number
+
+console.log(typeof (() => {}))   //выдаст function
+console.log(typeof [])   //выдаст object
+console.log(Array.isArray([]))   //выдаст true, вот так определяем, массив ли это
+
+// React - иммутабельная работа с данными, делаем копию данных а потом работаем с ней (как ниже не делать)
+const a = [1,2,3,4]  // в а хранится ссылка на ячейку памяти
+const b = a // b просто получило ссылку на массив выше
+console.log(b===a) // true, т.к. у них ссылка на один и тот же массив
+b.push(5) // если мы меняем b, то поменяется и a (т.к. у них ссылка на один и тот же объект)
+
+const c = [...a, 5] //диструктуризация массива, spred оператор
+// методы map и filter возвращают новый массив
