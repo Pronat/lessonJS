8a830290283102efe601c18f945a885dfbb26001
AlexPron <Fotolinza@gmail.com>
2023-05-10 20:35:12 +0300

    main11-3

--- src/lessonJSponedelnikSreda/main-11_Promise2.js
+++ src/lessonJSponedelnikSreda/main-11_Promise2.js
@@ -4,13 +4,26 @@ const server =  {
     getData() {
         return new Promise((res,rej)=> {
             setTimeout(()=>{
-                res('data from server')
+                // res('data from server')
                 rej('err from server')
             }, 1000)
         })
     }
 }
 
-server.getData().then((data)=> {
+server.getData()
+    .then((data)=> {
     console.log(data)
+})
+.catch((err)=> {
+    console.log(err)
+})
+
+
+fetch('https://google.com/query=js')
+.then(()=>{
+    return fetch('https://yahoo.com/query=js')          //если не сделать return, то все фетчи запустятся сразу, а так они запускаются только после выполнения предыдущего
+})
+.then(()=>{
+    return fetch('https://duckduckgo.com/query=js')
 })
