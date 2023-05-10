2e4ad26654a001b805e8bcacf54a09c3a765b63a
AlexPron <Fotolinza@gmail.com>
2023-05-10 15:16:07 +0300

    main11-2

--- src/lessonJSponedelnikSreda/main-11_Promise2.js
+++ src/lessonJSponedelnikSreda/main-11_Promise2.js
@@ -2,10 +2,15 @@ console.log('Promise2')
 
 const server =  {
     getData() {
-        const pr = new Promise((res,rej)=> {
+        return new Promise((res,rej)=> {
             setTimeout(()=>{
                 res('data from server')
+                rej('err from server')
             }, 1000)
         })
     }
 }
+
+server.getData().then((data)=> {
+    console.log(data)
+})
